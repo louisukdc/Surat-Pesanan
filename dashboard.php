be0aab6e9b930db581fc70f063d9a2ab901579d5
louisrkz <louis.maximillian@student.ukdc.ac.id>
2026-06-23 12:53:10 +0700

    new

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'order_form';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - RKZ Askes Systems</title>
+    <title>Dashboard - RKZ Askes System</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
