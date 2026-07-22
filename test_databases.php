6178af30628810bacb6871999dce5a2e134d15f3
louisrkz <louis.maximillian@student.ukdc.ac.id>
2026-07-22 14:01:47 +0700

    new update

--- test_databases.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/includes/auth.php';
-$r = mysqli_query($GLOBALS['db_conn'], 'SHOW DATABASES');
-while ($row = mysqli_fetch_array($r)) {
-    echo $row[0] . "\n";
-}
-?>
--- test_db_units.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/includes/auth.php';
-require_once dirname(__FILE__) . '/config/db_functions.php';
-$u = db_get_units();
-print_r($u);
-?>
